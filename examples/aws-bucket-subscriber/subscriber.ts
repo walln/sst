35f1f50c98c3689e2a752e2681540c4ea59be5ba
Mayowa Daniel <mayowadan17@gmail.com>
2024-11-09 00:10:33 +0100

    Add types to event argument (#5014)
    Quality of life improvement since we're using typescript

--- examples/aws-bucket-subscriber/subscriber.ts
+++ examples/aws-bucket-subscriber/subscriber.ts
@@ -1,4 +1,6 @@
-export const handler = async (event) => {
+import { Handler, S3Event } from 'aws-lambda';
+
+export const handler: Handler<S3Event> = async (event) => {
   console.log(event);
   return "ok";
 };
