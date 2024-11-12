982a630593d52ddc25827b75ae77c630ef1bdd7d
Dax Raad <d@ironbay.co>
2024-11-12 10:55:43 -0500

    sst.cloudflare.Worker: fix conflict with unenv polyfill

--- pkg/runtime/worker/unenv.mjs
+++ pkg/runtime/worker/unenv.mjs
@@ -1,6 +1,8 @@
-import { env, nodeless, cloudflare } from "unenv-nightly";
+import { env, nodeless, cloudflare } from "unenv-sst";
 const envConfig = env(nodeless, cloudflare, {});
 import { dirname, join } from "path";
 import { fileURLToPath } from "url";
 const __dirname = dirname(fileURLToPath(import.meta.url));
-Bun.write(join(__dirname, "./unenv.json"), JSON.stringify(envConfig, null, 2));
+let json = JSON.stringify(envConfig, null, 2);
+json = json.replaceAll("unenv/", "unenv-sst/");
+Bun.write(join(__dirname, "./unenv.json"), json);
