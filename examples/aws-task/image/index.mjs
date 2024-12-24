e790110a2bb5d59b777ba9579b4154ef29ee35c3
Dax Raad <d@ironbay.co>
2024-12-24 01:16:20 -0500

    wip: task

--- examples/aws-task/image/index.mjs
+++ examples/aws-task/image/index.mjs
@@ -1,3 +1,6 @@
 import { Resource } from "sst";
 
-console.log(`The bucket name is ${Resource.MyBucket.name}`);
+for (let i = 0; i < 10; i++) {
+  console.log(`The bucket name is ${Resource.MyBucket.name}`);
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+}
