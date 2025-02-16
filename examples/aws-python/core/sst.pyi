bf98e21a3fe4d2edd182c03b97b11be068db7dd2
Nick Wall <46641379+walln@users.noreply.github.com>
2025-02-16 16:01:30 -0500

    chore: update sst package path

--- examples/aws-python/core/sst.pyi
+++ examples/aws-python/core/sst.pyi
@@ -6,11 +6,4 @@ class Resource:
     class App:
         name: str
         stage: str
-    class MyLinkableValue:
-        foo: str
-        type: str
-    class MyPythonFunction:
-        name: str
-        type: str
-        url: str
 
